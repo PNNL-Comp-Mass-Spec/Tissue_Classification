39a5e3c5c4809070e79ae71a4767a543bebb1a5e
Kushner <irena.kushner@pnnl.gov>
2018-02-14 16:05:12 -0800

    Confusion matrix function

--- Classifiers/Classification_Utils.py
+++ Classifiers/Classification_Utils.py
@@ -2,13 +2,15 @@
 Provides functions to package together common classification steps
 """
 
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
 import re
-from sklearn import grid_search
-from sklearn import tree
+from sklearn import grid_search, tree
 from sklearn.decomposition import PCA, NMF
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, SelectPercentile
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import cross_val_predict, cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -352,3 +354,74 @@ Returns:
 """
 def fit_new_data(original_df, new_df):
     return original_df.iloc[:,:0].join(new_df)
+
+
+#########################
+#
+# Plotting
+#
+#########################
+
+"""
+From SKLearn ConfusionMatrix documentation
+http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
+"""
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+"""
+From SKLearn ConfusionMatrix documentation
+http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
+
+Args:
+    y_test (list of strings)
+    y_pred (list of strings)
+    groups (list of strings): list of all unique labels
+"""
+def show_confusion_matrices(y_test, y_pred, groups):
+    # Compute confusion matrix
+    cnf_matrix = confusion_matrix(y_test, y_pred)
+    np.set_printoptions(precision=2)
+
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=groups,
+                          title='Confusion matrix, without normalization')
+
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=groups, normalize=True,
+                          title='Normalized confusion matrix')
+
+    plt.show()
